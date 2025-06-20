dbeff5db1801a584a3ce9931717992bff061b708
Jason Sadowski <jasonsadowski552@gmail.com>
2025-06-20 14:55:45 -0500

    fix: moved job data modesl to a separate file

--- job_boards/linkedin.py
+++ job_boards/linkedin.py
@@ -1,29 +1,14 @@
 import os
-from typing import List, Optional
+from typing import List
 
 from dotenv import load_dotenv
 from apify_client import ApifyClient
-from pydantic import BaseModel, Field
 
+from datamodels.models import JobInfo
 
 load_dotenv()
 
 client = ApifyClient(token=os.getenv("APIFY_API_KEY"))
-
-# TODO: Move this to a models file
-class JobInfo(BaseModel):
-    company: str = Field("The company's name")
-    company_url: str = Field("LinkedIn Company URL")
-    description: str = Field("The job description")
-    is_verified: bool = Field("Whether the company has been verified")
-    job_title: str = Field("Job Title")
-    job_url: str = Field("LinkedIn Job URL")
-    location: str = Field("Location")
-    work_type: str = Field("OnSite/Remote/Hybrid status")
-    posted_at: str = Field("Posting date")
-    salary: str = Field("Provided salary information")
-    score: Optional[float] = Field("Assigned score by LLM")
-    explanation: Optional[str] = Field("Short explanation as to why the score was given") 
 
 
 def fetch_linkedin_posts(job_title: str, city: str, limit=5) -> List[JobInfo]:
