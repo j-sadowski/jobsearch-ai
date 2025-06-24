659559ed1a97767c22e97ed8f2ad94048b86201b
Jason Sadowski <jasonsadowski552@gmail.com>
2025-06-24 16:31:43 -0500

    fix: removed eval.py, moved display functions to main.py

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import argparse
+from datetime import datetime, timezone
+import json
 import logging
 from pathlib import Path
+from typing import List
 
+from datamodels.models import JobInfo
 from job_boards.linkedin import fetch_linkedin_posts
-from scoring.job_posts import score_job_posts
-from scoring.eval import evaluate_scores
+from scoring.job_posts import score_job_posts, identify_resume_gaps
 
 logging.basicConfig(
     level=logging.INFO,
@@ -12,6 +15,27 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 logger = logging.getLogger(__name__)
+
+CACHE_DIR = Path.cwd() / "data/cache"
+CACHE_DIR.mkdir(exist_ok=True)
+
+def display_output(scores: List[JobInfo], gap_summary: str, top_n=5) -> None:
+    dt_string = datetime.now(timezone.utc).strftime(format="%Y%m%d-%H%M%S")
+    sorted_jobs = sorted(scores, key=lambda x: x.score, reverse=True)
+
+    for job in sorted_jobs[:top_n]:
+        print("")
+        print(job.company)
+        print(f"{job.job_title}, score: {job.score}")
+        print(job.explanation)
+    jobs_d = {"query_date": dt_string, "jobs": [x.model_dump() for x in sorted_jobs]}
+    with open(CACHE_DIR / f"jobs_{dt_string}.json", "w") as f:
+        json.dump(jobs_d, f, indent=4)
+
+    print("")
+    print("Areas of improvement")
+    print(gap_summary)
+
 
 def run_workflow(resume: str, job_title: str, city: str, limit: int, hybrid:bool) -> None:
     """
@@ -25,7 +49,8 @@ def run_workflow(resume: str, job_title: str, city: str, limit: int, hybrid:bool
         1. Fetches LinkedIn job postings that match the specified job title and city.
            If 'hybrid' is True, only hybrid jobs in the search.
         2. Compares each job posting against the provided resume, assigning a score and reason.
-        3. Sorts the job postings by score, prints the top 5, saves all to cache
+        3. Summarize gaps in jobs > 7 that would improve resume
+        4. Sorts the job postings by score, prints the top 5, saves all to cache
 
     Args:
         resume (str): The contents of the user's resume in plain text.
@@ -42,7 +67,9 @@ def run_workflow(resume: str, job_title: str, city: str, limit: int, hybrid:bool
         logger.error("No job posts were returned from fetch. Exiting.")
         return
     scores = score_job_posts(resume, job_postings)
-    evaluate_scores(scores, top_n=5)
+    gap_summary = identify_resume_gaps(scores)
+    display_output(scores, gap_summary, top_n=5)
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="LLM based job searches given a prompt")
