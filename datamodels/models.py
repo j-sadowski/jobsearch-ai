ff20170707a0ef5d51e51e983b1a9e58a9f0a916
Jason Sadowski <jasonsadowski552@gmail.com>
2025-06-30 11:28:58 -0500

    fix: added data model to store required params

--- datamodels/models.py
+++ datamodels/models.py
@@ -1,16 +1,24 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 
+
+class WorkflowReqs(BaseModel):
+    resume: Optional[str] = Field(description="The plain text of a resume")
+    keywords: str = Field(description="The keywords to search in the job title and job description")
+    city: str = Field(description="The city where the job is located. DO NOT include the state")
+    limit: Optional[int] = Field(description="The maximum number of items to return", default=20)
+    hybrid: Optional[bool] = Field(description="Whether to apply a hybrid job-only filter", default=False)
+
 class JobInfo(BaseModel):
-    company: str = Field("The company's name")
-    company_url: str = Field("LinkedIn Company URL")
-    description: str = Field("The job description")
-    is_verified: bool = Field("Whether the company has been verified")
-    job_title: str = Field("Job Title")
-    job_url: str = Field("LinkedIn Job URL")
-    location: str = Field("Location")
-    work_type: str = Field("OnSite/Remote/Hybrid status")
-    posted_at: str = Field("Posting date")
-    score: Optional[float] = Field("Assigned score by LLM")
-    explanation: Optional[str] = Field("Short explanation as to why the score was given") 
+    company: str = Field(description="The company's name")
+    company_url: str = Field(description="LinkedIn Company URL")
+    description: str = Field(description="The job description")
+    is_verified: bool = Field(description="Whether the company has been verified")
+    job_title: str = Field(description="Job Title")
+    job_url: str = Field(description="LinkedIn Job URL")
+    location: str = Field(description="Location")
+    work_type: Optional[str] = Field(description="OnSite/Remote/Hybrid status", default="Remote")
+    posted_at: str = Field(description="Posting date")
+    score: Optional[float] = Field(description="Assigned score by LLM")
+    explanation: Optional[str] = Field(description="Short explanation as to why the score was given") 
 
