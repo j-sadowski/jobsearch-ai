8305b16144f4dbde869de099a61f4c3ff318642d
Jason Sadowski <jasonsadowski552@gmail.com>
2025-06-30 11:28:39 -0500

    feature: swapped UI to prompt based

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from typing import Dict, List
 
 from datamodels.models import JobInfo
 from job_boards.linkedin import fetch_linkedin_posts
+from scoring.oa_models import extract_reqs
 from scoring.job_posts import score_job_posts, identify_resume_gaps
 
 logging.basicConfig(
@@ -67,7 +68,7 @@ def display_output(scores: List[JobInfo], gap_summary: str, top_n=5) -> None:
     print(gap_summary)
 
 
-def run_workflow(resume: str, job_title: str, city: str, limit: int, hybrid:bool) -> None:
+def run_workflow(resume: str, prompt: str) -> None:
     """
     Executes the main workflow for job searching and evaluation.
 
@@ -87,34 +88,38 @@ def run_workflow(resume: str, job_title: str, city: str, limit: int, hybrid:bool
 
     Returns:
         None
-    """
+    """\
+    
+    search_data = extract_reqs(prompt)
+    return
+    search_data.resume = resume
     query_d = {
-        "keywords": job_title,
-        "city": city,
-        "hybrid": hybrid
+        "keywords": search_data.keywords,
+        "city": search_data.city,
+        "hybrid": search_data.hybrid
     }
-    job_postings = fetch_linkedin_posts(job_title, city, limit=limit, hybrid=hybrid)
+    job_postings = fetch_linkedin_posts(search_data)
     if len(job_postings) == 0:
         logger.error("No job posts were returned from fetch. Exiting.")
         return
-    scores = score_job_posts(resume, job_postings)
+    scores = score_job_posts(search_data.resume, job_postings)
     gap_summary = identify_resume_gaps(scores)
     display_output(scores, gap_summary, top_n=5)
     cache_data(query_d, scores, gap_summary)
     logger.info("Script complete")
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="LLM based job searches given a prompt")
     parser.add_argument("-r", "--resume_path", type=Path, help="Path to local resume, currently only .txt format", required=True)
-    parser.add_argument("-j", "--job_title", type=str, help="The job title to search for", required=True)
-    parser.add_argument("-c", "--city", type=str, default="Austin")
-    parser.add_argument("-l", "--limit", type=int, default=10)
-    parser.add_argument("-hyb", "--hybrid", action="store_true")
+    parser.add_argument("-p", "--prmopt", type=str, help="The LLM prompt", required=True)
     args = parser.parse_args()
 
     logger.info(f"Reading resume from {args.resume_path}")
-    with open(args.resume_path) as f:
-        resume = f.read()
-    logger.info(f"Got job search details looking for {args.job_title} jobs in {args.city}")
-
-    run_workflow(resume, args.job_title, args.city, args.limit, args.hybrid)
+    try:
+        with open(args.resume_path) as f:
+            resume = f.read()
+    except Exception as e:
+        logger.error(f"Unable to read resume! Exiting. {e}")
+        exit(1)
+    run_workflow(resume, args.prompt)
